f2099f6bc80982f4c472f3f1bf2f96f0f9904a37
stlife2012 <stlife2012@163.com>
2018-09-14 09:46:11 +0800

    todo_list 可运行列子

---
+++ std-ui-vue/std/todo_list/src/TodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <img src="./assets/logo.png">
+    <input v-model="inputValue"/>
+    <button @click="submitValue">提交</button><!-- v-on:click @click-->
+    <div>
+      <ul>
+        <!--<li v-for="(item,idx) of dataList" :key="idx">{{idx}} : {{item}}</li>-->
+        <todo-item
+          v-for="(item,idx) of dataList"
+          :content="item"
+          :index="idx"
+          :key="idx"
+          @delete_event="deleteValue"
+        >
+        </todo-item> <!-- 3.使用组件 --><!-- 1.父组件向子组件传值：设置传值参数 --><!-- 3.子组件向向组件传播事件：注册事件 @delete_event-->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import  TodoItem from './components/TodoItem' //1.导入组件
+  export default {
+    name: "TodoList",
+    components:{//2.定义组件
+      "todo-item":TodoItem
+    },
+    data() {
+      return {
+        inputValue: '',
+        dataList: []
+      }
+    },
+    methods:{
+      submitValue(){
+        this.dataList.push(this.inputValue)
+        this.inputValue = ''
+      },
+      deleteValue(index){
+        this.dataList.splice(index,1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ std-ui-vue/std/todo_list/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--<ul>-->
+    <!--<li v-for="(item,idx) in content" :key="idx" @click="removeItem">{{idx}} : {{item}}</li>&lt;!&ndash; 1.父组件向子组件传值：使用 &ndash;&gt;-->
+  <!--</ul>-->
+  <li @click="deleteEvent">{{index}} -> {{content}}</li><!--3.父组件向子组件传值：使用-->
+  <!--1.子组件向向组件传播事件：绑定事件-->
+</template>
+
+<script>
+  export default {
+    name: "TodoItem",
+    props:['content','index'],//2.父组件向子组件传值：绑定
+    data() {
+      return ""
+    },
+    methods:{
+      deleteEvent(){//1.子组件向向组件传播事件：注册事件
+        this.$emit('delete_event',this.index)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
