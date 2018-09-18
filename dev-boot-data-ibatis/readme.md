e38b4a778c552d889c2b81623e98b2c6eb15e461
stlife2012 <stlife2012@163.com>
2018-09-18 13:29:11 +0800

    实现了一个task_info表的分页查询

--- dev-boot-data-ibatis/readme.md
+++
@@ -1,8 +0,0 @@
-### 概述
-    配置mysql数据库源，通过mybaits插件自动生成mapper文件和pojo文件
-    
-### 注意事项
-- Mapper扫描范围采用@Mapper进行注释
-
-### 测试
-- http://localhost:8080/data/task/
--- dev-boot-data-ibatis/src/main/resources/sql/table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `task_info` (
-  `uid`         varchar(100) NOT NULL,
-  `remark`      varchar(200)      DEFAULT NULL,
-  `create_time` timestamp    NULL DEFAULT NULL,
-  `create_ac`   varchar(100)      DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = latin1
